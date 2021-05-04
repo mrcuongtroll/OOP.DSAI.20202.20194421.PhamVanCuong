21a4d5e1a57110a607daad62b47d4c475f77d517
mrcuongtroll <38219186+mrcuongtroll@users.noreply.github.com>
2021-05-04 16:53:18 +0700

    I forgot to add the getLength()

--- AimsProject/src/hust/soict/dsai/aims/media/Track.java
+++ AimsProject/src/hust/soict/dsai/aims/media/Track.java
@@ -4,6 +4,15 @@ public class Track {
 	private String title;
 	private int length;
 	
+	public boolean equals(Object track) {
+		Track that = (Track) track;
+		if ((that instanceof Track) && (this.length == that.length) && (this.title.toLowerCase().equals(that.getTitle().toLowerCase()))) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 	public Track(String title, int length) {
 		super();
 		this.title = title;
